1dd669b3be245bee79c71a598efc217290abe099
fereshteh_dehghani <fdehghani666@gmail.com>
2023-12-04 16:10:33 +0330

    change layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,46 @@
 import { ClerkProvider } from '@clerk/nextjs'
- 
+import type { Metadata } from 'next'
+import {Inter,Space_Grotesk} from "next/font/google"
+
+const inter = Inter({
+  subsets:['latin'],
+  weight:['100','200','300','400',
+'500','600','700','800','900'],
+variable:'--font-inter'
+})
+
+const spaceGrotesk = Space_Grotesk({
+  subsets:['latin'],
+  weight:['300','400','500','600','700'],
+variable:'--font-spaceGrotesk'
+})
+
+export const metadata: Metadata = {
+  title: '...',
+  description: 'A Community-drivrn platform for asking and answering programming questions get help, share knowledge and collaborate with Developers from around the world explore mobile app development algorithms data researchers and more',
+icons:{
+  icon:'/assets/images/site-logo.svg'
+}
+}
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+    appearance ={{
+      elements:{
+        formButtonPrimary :"primary-gradient",
+        footerActionLink:"primary-text-gradient hover:text-gradient-500",
+      }
+    }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+          </body>
  
       </html>
     </ClerkProvider>
