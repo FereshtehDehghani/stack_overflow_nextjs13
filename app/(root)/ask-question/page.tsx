a3a71a2521be44c03fadd9a694bf5be8fddbbf2c
FereshtehDehghani <fereshtehdehghani963@gmail.com>
2023-12-26 09:59:39 +0330

    create question form

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import Question from "@/components/forms/Question"
+import React from "react"
 
-const page = () => {
+const Page = () => {
   return (
     <div>
-      ASK QUESTION
+      <h1 className="h1-bold taxt-dark100_light900">
+        Ask a question
+      </h1>
+     <div className="mt-9">
+     <Question/>
+      </div> 
     </div>
   )
 }
 
-export default page
+export default Page
